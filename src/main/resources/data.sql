8981fd59b4381cdda8884b865d16e5277e3e0dda
Oleksandr Korshunovych <oleksandr_korshunovych@epam.com>
2023-03-26 03:41:20 +0200

    add data.sql, data csv files

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+COPY person FROM 'C:\Temp\data\persons.csv' DELIMITER ',' CSV HEADER;
+COPY subject FROM 'C:\Temp\data\subjects.csv' DELIMITER ',' CSV HEADER;
+COPY student FROM 'C:\Temp\data\students.csv' DELIMITER ',' CSV HEADER;
+COPY tutor FROM 'C:\Temp\data\tutors.csv' DELIMITER ',' CSV HEADER;
+COPY tutors_subjects FROM 'C:\Temp\data\tutors_subjects.csv' DELIMITER ',' CSV HEADER;
+COPY exam_results FROM 'C:\Temp\data\exam_results.csv' DELIMITER ',' CSV HEADER;
+
+
